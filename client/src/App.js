d2961150620912003be888557440aaad88c49d13
Sherline Au <au.sherline@gmail.com>
2023-01-29 11:27:42 -0600

    material ui setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+  // grabs mode from state/index.js
+  const mode = useSelector((state) => state.global.mode);
+
+  // passes theme.js into createTheme function
+  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
+
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      {/* resets all css */}
+      <CssBaseline />
+    </ThemeProvider>
+  </div>;
 }
 
 export default App;
