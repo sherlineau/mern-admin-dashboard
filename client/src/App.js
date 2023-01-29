d4e4365f99d8998798a26c6184682d4b665fc050
Sherline Au <au.sherline@gmail.com>
2023-01-29 12:30:35 -0600

    setup layout and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,35 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { themeSettings } from "theme";
+import Dashboard  from 'scenes/dashboard'
+import Layout from 'scenes/layout'
 
 function App() {
   // grabs mode from state/index.js
   const mode = useSelector((state) => state.global.mode);
 
   // passes theme.js into createTheme function
-  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  return <div className="app">
-    <ThemeProvider theme={theme}>
-      {/* resets all css */}
-      <CssBaseline />
-    </ThemeProvider>
-  </div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* resets all css */}
+          <CssBaseline />
+          <Routes>
+            {/* default element for all routes is Layout component.  Everyone route will have a layout (with a navbar/footer) and the children inside it */}
+            <Route element={<Layout />}> 
+              <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
